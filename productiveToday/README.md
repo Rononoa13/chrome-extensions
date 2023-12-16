3ebb781b47624f4b2f6f60ebe774f91f45e4771b
Rononoa13 <sumitluitel42@gmail.com>
2023-12-16 23:11:33 +0545

    Fix different window tab open + update manifest v3

---
+++ productiveToday/README.md
@@ -0,0 +1,18 @@
+Chrome Extension: [Extension Name]
+
+Overview
+
+Features
+
+Total Tabs Display: Shows the total number of open tabs in the current window.
+Clickable List: Displays a clickable list of open tabs with the ability to switch between them.
+
+Installation
+Clone this repository to your local machine.
+
+git clone https://github.com/your-username/your-extension.git
+Open Chrome and navigate to chrome://extensions/.
+
+Enable "Developer mode" in the top right corner.
+
+Click "Load unpacked" and select the folder where you cloned the repository.
--- productiveToday/popup.js
+++ productiveToday/popup.js
@@ -5,6 +5,10 @@ chrome.tabs.query({}, function (tabs) {
     document.getElementById('total_tabs').textContent += " " + tabsNumber
 })
 
+const findTabs = (data) => {
+    return data.length
+}
+
 // Get element <h6 id="url_links">Links:</h6> from popup.html
 let myList = document.getElementById('url_links')
 // Create new element <ul> </ul>
@@ -12,30 +16,47 @@ let myUlList = document.createElement('ul')
 // Create this: <li> {text} </li>
 function createListItem(text) {
     let li = document.createElement('li')
-    li.innerHTML = text
+    li.innerHTML = `<a href='#'>${text}</a>`
     return li
 }
 // Append <ul> tag inside of <h6> tag in popup.html
 appendChildren(myList, myUlList)
 
-// returns all the tabs in windows
-chrome.tabs.query({}, function (tabs) {
-    tabs.reverse().forEach(function (element) {
-        // Append <li> tag inside of <ul> tag in popup.html
-        let myListItem = createListItem(`<a href='#'>` + `${element.title}` + `</a>`)
-        appendChildren(myUlList, myListItem)
-        // Click on links to do something
-        myListItem.onclick = function () {
-            // Active tab on click
-            chrome.tabs.update(element.id, { active: true })
-        }
-    })
-})
-
 // Function to append children element to parent element
 function appendChildren(parent, children) {
     parent.appendChild(children)
 }
+// returns all the tabs in windows
+chrome.tabs.query({}, function (tabs) {
+    tabs.reverse().forEach(function (element) {
+        // Append <li> tag inside of <ul> tag in popup.html
+        let myListItem = createListItem(element.title)
+        appendChildren(myUlList, myListItem)
+        // Click on links to do something
+        myListItem.onclick = function () {
+            chrome.windows.getCurrent(function (currentWindow) {
+                if (currentWindow.id === element.windowId) {
+                    chrome.tabs.update(element.id, { active: true })
+            // } else {
+                chrome.windows.update(element.windowId, { focused: true }, function () {
+                            // Activate the tab within the window
+                            chrome.tabs.update(element.id, { active: true });
+                })}
+            })
+            // let windowId = element.windowId;
+            
+            // if (windowId === chrome.windows.WINDOW_ID_CURRENT) {
+            //     chrome.tabs.update(element.id, { active: true })
+            // } else {
+            //     chrome.windows.update(element.windowId, { focused: true }, function () {
+            //                 // Activate the tab within the window
+            //                 chrome.tabs.update(element.id, { active: true });
+            // })}
+        }
+    })
+})
+
+
 
 // Checks if a particular URL is present.
 // And close facebook if present.
@@ -48,9 +69,7 @@ chrome.tabs.query({
     // TODO: close the tab if facebook is present.
 });
 
-findTabs = (data) => {
-    return data.length
-}
+
 // findURL = (data) => {
 //     return data.url === "*://*.youtube.com/*"
 // }
